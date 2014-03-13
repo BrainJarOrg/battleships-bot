278f26849a5c17453d1694b8d9f17ad42520ad33
Mike Sokolsky <mike@udacity.com>
2014-03-13 16:58:58 +0100

    Ready to test random bot

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import json
+import random
 import sys
 
 
@@ -26,17 +27,40 @@ def place_ships():
 
 class Bot(object):
 
-    def __init__(self, ai='repeat'):
-        self.play = getattr(self, 'ai')
+    def __init__(self, ai='repeat_bot'):
+        self.play = getattr(self, ai)
+        random.seed()
 
-    def repeat(self, state):
-        move = {"move": "11"}
+    def construct_state(self, state):
+        self.state = [[0, 0, 0, 0, 0, 0, 0, 0] for x in range(8)]
+        self.free = []
+        for x in range(8):
+            for y in range(8):
+                self.free.append((x, y))
+        for move in state['hit']:
+            move = tuple(map(int, move))
+            x, y = move
+            self.state[x][y] = 1
+            self.free.remove(move)
+        for move in state['missed']:
+            move = tuple(map(int, move))
+            x, y = move
+            self.state[x][y] = -1
+            self.free.remove(move)
+
+    def random_move(self):
+        move = self.free.pop(random.randint(0, len(self.free) - 1))
+        return "".join(map(str, move))
+
+    def repeat_bot(self, state):
+        self.construct_state(state)
+        move = {'move': self.random_move()}
         print json.dumps(move)
 
 
 if __name__ == "__main__":
     state = json.loads(sys.argv[1])
-    if state['cmd']:
+    if state['cmd'] == 'init':
         place_ships()
     else:
         bot = Bot()
